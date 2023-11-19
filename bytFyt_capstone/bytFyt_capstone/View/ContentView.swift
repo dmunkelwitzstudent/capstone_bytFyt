c4048dacb2563bdefab3df674319896052356b94
Andrew Thayer <athayer2@cvtc.edu>
2023-11-18 19:41:56 -0600

    added SleepView, updated ContentView

--- bytFyt_capstone/bytFyt_capstone/View/ContentView.swift
+++ bytFyt_capstone/bytFyt_capstone/View/ContentView.swift
@@ -8,18 +8,38 @@
 import SwiftUI
 import SwiftData
 
+//ContentView is the "main" view for the app, defaulted to "
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
-    var body: some View {
-      
+  // Keep track of which tab is selected
+  @State private var selectedTab = "Dashboard"
+  
+  var body: some View {
+    TabView(selection: $selectedTab) {
+      //View for dashboard
       DashboardView()
-       
+        .tabItem {
+          Label("Dashboard", systemImage: "house")
+        }
+      //Tags as "Dashboard" -- similar to ID
+        .tag("Dashboard")
+      //View for sleep
+      SleepView()
+        .tabItem {
+          Label("Sleep", systemImage: "moon.zzz")
+        }
+        .tag("Sleep")
+      
+      //Add more tabs below when they're done
     }
+    //Sets color of tabs
+    //TODO: figure out why only selected tab is white...
+    .accentColor(.white)
+  }
 }
 
-#Preview {
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+  }
 }
---
+++ bytFyt_capstone/bytFyt_capstone/View/SleepView.swift
@@ -0,0 +1,18 @@
+//
+//  SleepView.swift
+//  bytFyt_capstone
+//
+//  Created by Andrew Thayer on 11/18/23.
+//
+
+import SwiftUI
+
+struct SleepView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SleepView()
+}
